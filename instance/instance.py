e230c70a35c871ded5e6cc6217adb0b81bfb1546
Stefan Vitanov <fractalexperience@mail.bg>
2021-03-23 22:53:23 +0200

    Added instance modules

---
+++ instance/instance.py
@@ -0,0 +1,44 @@
+from xbrl.instance import m_xbrl
+from xbrl.base import fbase, const
+from xbrl.ixbrl import m_ixbrl
+from lxml import etree as lxml
+
+
+class Instance(fbase.XmlFileBase):
+    def __init__(self, location=None, container_pool=None):
+        self.pool = container_pool
+        self.taxonomy = None
+        self.xbrl = None
+        self.ixbrl = None
+        parsers = {
+            f'{{{const.NS_XHTML}}}html': self.l_ixbrl,
+            f'{{{const.NS_XBRLI}}}xbrl': self.l_xbrl
+        }
+        if location:
+            self.location = location
+            super().__init__(location, container_pool, parsers)
+
+    def l_xbrl(self, e):
+        self.xbrl = m_xbrl.XbrlModel(e, self)
+
+    def l_ixbrl(self, e):
+        self.ixbrl = m_ixbrl.IxbrlModel(e, self)
+        self.ixbrl.strip()
+        root = lxml.XML(''.join(self.ixbrl.output))
+        self.xbrl = m_xbrl.XbrlModel(root, self)
+
+    def to_xml(self):
+        return self.ixbrl.to_xml() if self.ixbrl else self.xbrl.to_xml() if self.xbrl else None
+
+    def info(self):
+        return f'''Namespaces: {len(self.namespaces)}
+Schema references: {len(self.xbrl.schema_refs)}
+Linkbase references: {len(self.xbrl.linkbase_refs)}
+Contexts: {len(self.xbrl.contexts)}
+Units: {len(self.xbrl.units)}
+Facts: {len(self.xbrl.facts)}
+Footnotes: {len(self.xbrl.footnotes)}
+Filing Indicators: {len(self.xbrl.filing_indicators)}
+'''
+
+
---
+++ instance/unit.py
@@ -0,0 +1,42 @@
+from xbrl.base import ebase, const
+
+
+class Unit(ebase.XmlElementBase):
+    def __init__(self, e):
+        self.measure = []
+        self.numerator = []
+        self.denominator = []
+        self.active_measure_list = self.measure
+        parsers = {
+            f'{{{const.NS_XBRLI}}}unit': self.l_unit,
+            f'{{{const.NS_XBRLI}}}measure': self.l_measure,
+            f'{{{const.NS_XBRLI}}}divide': self.l_divide,
+            f'{{{const.NS_XBRLI}}}unitNumerator': self.l_numerator,
+            f'{{{const.NS_XBRLI}}}unitDenominator': self.l_denominator
+        }
+        super().__init__(e, parsers)
+
+    def l_unit(self, e):
+        self.l_children(e)
+
+    def l_measure(self, e):
+        self.active_measure_list.append(e)
+
+    def l_numerator(self, e):
+        self.active_measure_list = self.numerator
+        self.l_children(e)
+
+    def l_denominator(self, e):
+        self.active_measure_list = self.denominator
+        self.l_children(e)
+
+    def l_divide(self, e):
+        self.l_children(e)
+
+    def get_aspect_value(self):
+        if self.measure:
+            return ','.join([m.text for m in self.measure])
+        else:
+            num = ','.join(m.text for m in self.numerator)
+            den = ','.join(m.text for m in self.denominator)
+            return f'{num}/{den}'
