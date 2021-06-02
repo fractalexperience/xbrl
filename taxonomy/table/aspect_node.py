8564a4c17af9ee8c608d89b7d5904c2793863456
Stefan Vitanov <fractalexperience@mail.bg>
2021-06-02 22:15:18 +0300

    Added aspect node parsing

--- taxonomy/table/aspect_node.py
+++ taxonomy/table/aspect_node.py
@@ -4,8 +4,12 @@ from xbrl.taxonomy.table import def_node, str_node
 class AspectNode(def_node.DefinitionNode):
     """ Implements an aspect node """
     def __init__(self, e, container_xlink=None):
+        self.aspects = {}
         super().__init__(e, container_xlink)
+        for e2 in e.iterchildren():
+            self.aspects[e2.tag.split('}')[1]] = e2.text
 
     def compile(self, names=None, lvl=0, s_node=None):
         sn = str_node.StructureNode(s_node, self)
         super().compile(names, lvl, sn)
+
--- taxonomy/table/rule_node.py
+++ taxonomy/table/rule_node.py
@@ -1,4 +1,5 @@
 from xbrl.taxonomy.table import def_node, str_node
+from xbrl.base import const
 from xbrl.base import data_wrappers
 
 
@@ -9,7 +10,19 @@ class RuleNode(def_node.DefinitionNode):
         self.is_abstract = abst is not None and abst.lower() in ['true', '1']
         merg = e.attrib.get('merge')
         self.is_merged = merg is not None and merg.lower() in ['true', '1']
+        self.rule_sets = {'default': {}}
         super().__init__(e, container_xlink)
+        for e2 in e.iterchildren():
+            if e2.tag == f'{{{const.NS_TABLE}}}ruleSet':
+                name = e2.attrib.get('tag')
+                for e3 in e2.iterchildren():
+                    self.l_restrictions(e3, name)
+            else:
+                self.l_restrictions(e2, 'default')
+
+    def l_restrictions(self, e, rule_set_name):
+        restrictions = self.rule_sets.setdefault(rule_set_name, {})
+        restrictions[]
 
     def compile(self, names=None, lvl=0, s_node=None):
         sn = str_node.StructureNode(s_node, self, self.is_merged)
