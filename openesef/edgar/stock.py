6d043de4746c30b9a9daf12327f0db8ed5ee9fd3
reeyarn <reeyarn@gmail.com>
2025-02-18 16:03:07 +0100

    Update stock.py

--- openesef/edgar/stock.py
+++ openesef/edgar/stock.py
@@ -8,8 +8,8 @@ This module ties it all together; it will be the main module that's used
 
 '''
 import pandas as pd
-from openesef.edgar import get_financial_filing_info, get_latest_quarter_dir, find_latest_filing_info_going_back_from, EG_LOCAL #,SYMBOLS_DATA_PATH
-from openesef.filing import Filing
+from openesef.edgar.edgar import get_financial_filing_info, get_latest_quarter_dir, find_latest_filing_info_going_back_from, EG_LOCAL #,SYMBOLS_DATA_PATH
+from openesef.edgar.filing import Filing
 from datetime import datetime
 #import pandas as pd 
 import os
